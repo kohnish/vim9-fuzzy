a3cf7a6dfb57925c7d16901a45f3e037c17ba9b2
kohnish <kohnish@gmx.com>
2023-08-17 21:10:51 +0200

    grep breaking on non-ascii

--- native/app/test-vim9-fuzzy.cxx
+++ native/app/test-vim9-fuzzy.cxx
@@ -3,5 +3,30 @@
 #include "search_helper.h"
 #include "vim9_fuzzy_env.h"
 #include <gtest/gtest.h>
+#include <string>
 
-TEST(fuzzy, start_fuzzy_response) {}
+
+// const std::string g_test_str = std::string("aあiい");
+const char *s = "aあiい";
+// 2 bytes: 110xxxxx 10xxxxxx
+// 192
+// 3 bytes: 1110xxxx 10xxxxxx 10xxxxxx
+// 4 bytes: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
+TEST(fuzzy, start_fuzzy_response) {
+    if ((s[1] & 192) >= 192) {
+        printf("two bytes\n");
+    }
+    if ((s[1] & 224) >= 224) {
+        printf("three bytes\n");
+    }
+    if ((s[1] & 240) >= 240) {
+        printf("four bytes \n");
+    }
+    // printf("%02X\n", g_test_str.c_str()[0]);
+    // printf("%02X\n", g_test_str.c_str()[1]);
+    // printf("%02X\n", g_test_str.c_str()[2]);
+    // printf("%02X\n", g_test_str.c_str()[3]);
+    // printf("%02X\n", g_test_str.c_str()[4]);
+    // printf("%02X\n", g_test_str.c_str()[5]);
+}
